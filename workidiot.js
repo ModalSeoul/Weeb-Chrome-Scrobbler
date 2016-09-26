111627ec39fbc734f1f45bc95c203c53afd75d13
ModalSeoul <Algernon@teknik.io>
2016-09-26 11:32:17 -0600

    initial

---
+++ workidiot.js
@@ -0,0 +1,9 @@
+/*
+ *  This is entirely stupid, I know that.
+ *  But I have problems focusing, man.
+ *  Hence workidiot.js
+ *  I need to work, idiot.
+ */
+alert('Work, idiot.');
+window.location.href = 'http://google.com';
+
